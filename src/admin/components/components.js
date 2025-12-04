03105077a6dcd467a0fe4a3e174b152716f9921a
vitorbanuth <phobinhoo5@gmail.com>
2025-12-04 12:48:06 -0300

    fix: removed components

--- src/admin/components/components.js
+++ src/admin/components/components.js
@@ -5,31 +5,9 @@ const componentLoader = new ComponentLoader();
 const Components = {
   Edit: componentLoader.add("Edit", "./upload-image.edit.jsx"),
   List: componentLoader.add("List", "./upload-image.list.jsx"),
-  DownloadSinglePdf: componentLoader.add(
-    "DownloadSinglePdf",
-    "./pdfs/downloadSinglePdf.jsx"
-  ),
-  DownloadAllPdfs: componentLoader.add(
-    "DownloadAllPdfs",
-    "./pdfs/downloadAllPdfs.jsx"
-  ),
-  DownloadSelectedPdfs: componentLoader.add(
-    "DownloadSelectedPdfs",
-    "./pdfs/downloadSelectedPdfs.jsx"
-  ),
+  
   HomePage: componentLoader.add("HomePage", "./homePage.jsx"),
-  DownloadSingleCsv: componentLoader.add(
-    "DownloadSingleCsv",
-    "./csvs/downloadSingleCsv.jsx"
-  ),
-  DownloadSelectedCsvs: componentLoader.add(
-    "DownloadSelectedCsvs",
-    "./csvs/downloadSelectedCsvs.jsx"
-  ),
-  DownloadAllCsvs: componentLoader.add(
-    "DownloadAllCsvs",
-    "./csvs/downloadAllCsvs.jsx"
-  ),
+  
 };
 
 export { componentLoader, Components };
